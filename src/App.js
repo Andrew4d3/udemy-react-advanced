a315d5a4bde0101e74d76678ae3eeb3f5165fe4f
Andrew Davis <andrew4d3@gmail.com>
2019-04-29 10:23:08 -0400

    Changing logo to PNG

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
+  const logo =
+    "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png";
   return (
     <div className="App">
       <header className="App-header">
