7d4311ec8bb2d791321c3e18870593e36e9f4bdf
Andrew4d3 <andrew4d3@gmail.com>
2019-05-01 01:14:01 -0400

    Section 2 - Lecture 5

--- src/App.test.js
+++ src/App.test.js
@@ -10,3 +10,12 @@ it('renders without crashing', () => {
 
   ReactDOM.unmountComponentAtNode(div);
 });
+
+/*
+  So what are we doing here?
+  1. First we run "npm run test" at the terminal.
+  2. Then Jest test runner starts up
+  3. It will go through all the *.test.js files inside the src folder and execute the tests inside of them
+  4. It will print the results
+  5. After printing the results, the Jest runner will keep watching the files for changes and re-executing the tests on the files that change.
+*/
