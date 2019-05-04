f5cf6837022f9377b1edcdf7834ab148b73d1275
Andrew4d3 <andrew4d3@gmail.com>
2019-05-03 21:31:18 -0400

    Section 2 - Lecture 12

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React from "react";
+import CommentBox from "./CommentBox";
+import CommentList from "./CommentList";
 
 // For the moment, we are just rendering some text in the main App component
 export default () => {
-  return <div>I am the component</div>;
+  return (
+    <div>
+      <CommentBox />
+      <CommentList />
+    </div>
+  );
 };
