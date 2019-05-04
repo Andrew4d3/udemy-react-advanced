a6765d9901a0ca011f4989db8b157caed49928ad
Andrew4d3 <andrew4d3@gmail.com>
2019-05-03 21:17:51 -0400

    Section 2 - Lecture 11

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+// For the moment, we are just rendering some text in the main App component
+export default () => {
+  return <div>I am the component</div>;
+};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+
+// Here we are linking the main App component with the index HTML file
+ReactDOM.render(<App />, document.querySelector("#root"));
