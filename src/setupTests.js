2825e5bdd6325f7dbade5d59563e4b931f85e542
Andrew Davis <reign-andrew@Andrews-MacBook-Pro.local>
2019-05-14 19:09:07 -0400

    Section 2 - Lecture 20

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,5 @@ import Adapter from 'enzyme-adapter-react-16';
 
 // With this we're wiring up Enzyme into our Project
 Enzyme.configure({ adapter: new Adapter()});
+
+// Check commit comments to se notes for this lecture
