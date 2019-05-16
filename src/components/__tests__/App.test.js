da57537104b2eb5593ca857a1f1015f989ff382c
Andrew4d3 <andrew4d3@gmail.com>
2019-05-15 23:41:27 -0400

    Section 2 - Lecture 21

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,20 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import App from "../App";
+import CommentBox from "../CommentBox";
 
 it("shows a comment box", () => {
-  // Here we're creating a fake div to use in our test
-  // We aren't using any web engine here, this only exists in memory
-  const div = document.createElement("div");
+  // With shallow we are rendering only the single component, not including its children.
+  const wrapped = shallow(<App />);
 
-  // Mounting the App component we want to test inside our fake div
-  ReactDOM.render(<App />, div);
-
-  // Looks inside the div
-  // and checks to see if the CommentBox is in there
-  expect(div.innerHTML).toContain("Comment Box");
-  // Why is this expect statement wrong?: https://github.com/Andrew4d3/udemy-react-advanced/issues/5
-
-  // Unmounting the App component we just mounted, (doing a "clean-up")
-  ReactDOM.unmountComponentAtNode(div);
+  // Now we check if the wrapped component has an instance of CommentBox (we don't care the CommentBox content)
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
