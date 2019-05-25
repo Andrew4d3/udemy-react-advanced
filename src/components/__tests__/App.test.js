2b6ad9f347865dde16fee111223f82501b40e97c
Andrew4d3 <andrew4d3@gmail.com>
2019-05-25 15:12:15 -0400

    Section 2 - Lecture 25  - Code Reuse with BeforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,17 +3,19 @@ import { shallow } from "enzyme";
 import App from "../App";
 import CommentBox from "components/CommentBox"; // Now we can use absilute paths because we define a .env file that changes the default NODE_PATH
 import CommentList from "components/CommentList";
+let wrapped;
+
+// In order to avoid repeating code, we can define a "beforeEach" call, which will execute before each of the "it" calls
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
 it("shows a comment box", () => {
-  // With shallow we are rendering only the single component, not including its children.
-  const wrapped = shallow(<App />);
-
-  // Now we check if the wrapped component has an instance of CommentBox (we don't care the CommentBox content)
+  // We used the globally defined wrapped here
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it("shows a comment list", () => {
   // We are doing the same with the CommentList component
-  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentList).length).toEqual(1);
 });
