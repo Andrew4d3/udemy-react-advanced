8006b9d39dfb2a6645da215ed1cdcda2a101c6e0
Andrew4d3 <andrew4d3@gmail.com>
2019-05-25 14:59:36 -0400

    Section 2 - Lecture 24 - Absolute Path Imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import CommentBox from "./CommentBox";
-import CommentList from "./CommentList";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
 
 // For the moment, we are just rendering some text in the main App component
 export default () => {
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "../App";
-import CommentBox from "../CommentBox";
-import CommentList from "../CommentList";
+import CommentBox from "components/CommentBox"; // Now we can use absilute paths because we define a .env file that changes the default NODE_PATH
+import CommentList from "components/CommentList";
 
 it("shows a comment box", () => {
   // With shallow we are rendering only the single component, not including its children.
