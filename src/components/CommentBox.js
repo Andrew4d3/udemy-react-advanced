6e5cf842253623efe2c988033bcacf6bdf381fed
Andrew4d3 <andrew4d3@gmail.com>
2019-05-25 15:23:25 -0400

    Section 2 - Lecture 26 - Comment Box Component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default () => {
-  return <div>Comment Box</div>;
-};
+// Just writing the CommentBox component. Nothing else...
+class CommentBox extends Component {
+  render() {
+    return (
+      <form>
+        <h4>Add a comment</h4>
+        <textarea />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default CommentBox;
