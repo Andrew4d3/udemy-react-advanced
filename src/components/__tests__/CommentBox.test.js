78962a9fe0bd1f7d9310e7a2adc69048dd156082
Andrew4d3 <andrew4d3@gmail.com>
2019-05-25 20:02:04 -0400

    Section 2 - Lecture 29 - Asserting Elements Existence

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,4 +1,13 @@
 import React from "react";
+import { mount } from "enzyme";
 import CommentBox from "components/CommentBox";
 
-it("shows a comment box", () => {});
+it("shows a comment box", () => {
+  // As I mentioned, we are testing the full DOM, we need to use "mount" for that
+  const wrapped = mount(<CommentBox />);
+
+  // With "find", we cannot only find component instances but HTML components as well.
+  // So we can verify the existence of the following HTML components: (textarea, button)
+  expect(wrapped.find("textarea").length).toEqual(1);
+  expect(wrapped.find("button").length).toEqual(1);
+});
