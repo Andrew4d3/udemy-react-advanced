f2f221a08535a20f1df3419161abcfa1aebe72a2
Andrew4d3 <andrew4d3@gmail.com>
2019-05-25 20:30:41 -0400

    Section 2 - Lecture 30 - AfterEach Statements

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { mount } from "enzyme";
 import CommentBox from "components/CommentBox";
+let wrapped;
+
+beforeEach(() => {
+  // As I mentioned, we are testing the full DOM, we need to use "mount" for that
+  wrapped = mount(<CommentBox />);
+});
+
+// Since we're using a full DOM testim, it's neccessary to do a "clean-up" after each test runs
+// We only have to use the "unmount" method
+afterEach(() => {
+  wrapped.unmount();
+});
 
 it("shows a comment box", () => {
-  // As I mentioned, we are testing the full DOM, we need to use "mount" for that
-  const wrapped = mount(<CommentBox />);
-
   // With "find", we cannot only find component instances but HTML components as well.
   // So we can verify the existence of the following HTML components: (textarea, button)
   expect(wrapped.find("textarea").length).toEqual(1);
