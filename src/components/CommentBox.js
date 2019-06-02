4d406e8768573ce96b8ab2d49ff3d0fdcfd81ddd
Andrew4d3 <andrew4d3@gmail.com>
2019-06-02 19:17:31 -0400

    Section 2 - Lecture 41 - Bonding react with redux

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+// (1) We need to connect this component to redux, so let's import the required dependencies
+import { connect } from "react-redux";
+import * as actions from "actions"; // (2) And here we're importing all our action dispatchers
 
 // Now it's time to set the event handlers
 class CommentBox extends Component {
@@ -10,6 +13,8 @@ class CommentBox extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
+    // (4) And here we call the saveComment action dispatcher, with the current entered comment as parameter
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: "" });
   };
 
@@ -26,4 +31,13 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+// (3) We wrapp our component around the connect method from redux
+// It receives two function parameters: mapStateToProps and mapDispatchToProps
+// This component doesn't utilize a redux state so we just pass null
+// As second parameter we pass the imported action dispatchers
+export default connect(
+  null,
+  actions
+)(CommentBox);
+
+// (5) Everything seems to be working OK in our app. But now our tests are failing. What could go wrong?
