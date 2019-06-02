3b85a11d9d5c1ccb037b53ca902fd074576f15f9
Andrew4d3 <andrew4d3@gmail.com>
2019-06-02 18:39:21 -0400

    Section 2 - Lecture 39 - The provider tag

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// Now it's time to connect the redux setup with our react app.
+// For this, first we need to import the corresponding dependencies
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "reducers";
 
 import App from "./components/App";
 
-// Here we are linking the main App component with the index HTML file
-ReactDOM.render(<App />, document.querySelector("#root"));
+// Now we have to wrap our main app with the Provider component from react redux
+// We set the corresponding store props using the createStore method from redux,
+// which will take as argument the main reducer we defined before and a initial state (empty object)
+ReactDOM.render(
+  <Provider store={createStore(reducers, {})}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
