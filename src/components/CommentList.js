2e6ad3a37ba0c55e23669a7bb197f2f11f13c286
Andrew Davis <reign-andrew@Andrews-MacBook-Pro.local>
2019-08-13 09:34:37 -0400

    Section 2 - Lecture 48 - Comment List Wireup

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from 'react-redux';
 
-export default () => {
-  return <div>Comment List</div>;
-};
+//(1) Let's refactor this component to make it work as a class rather than as a function
+
+class CommentList extends Component {
+  // (3) This is the class method that will populate the list based on the props received
+  renderComments() {
+    return this.props.comments.map(comment => {
+      return <li key={comment}>{comment}</li>
+    })
+  }
+
+  render() {
+    // (2) Let's define the render method by displaying a HTML list, such list will be populated with another method
+    return (
+      <div>
+        <ul>
+          {this.renderComments()}
+        </ul>
+      </div>
+    )
+  }
+}
+
+// (4) We need to map the comments from the global state to the props component
+function mapStateToProps(state) {
+  return { comments: state.comments }
+}
+
+// (5) And connect everything
+export default connect(mapStateToProps)(CommentList);
