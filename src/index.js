cdef97e11b717c888bfcca3df1e92267f0d09ab1
Andrew4d3 <andrew4d3@gmail.com>
2019-08-20 22:18:28 -0400

    Section 3 - Lecture 68 - Adding React Router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+// (1) Install and import the react-router-dom dependency. We need to import these two methods
+import { BrowserRouter, Route } from "react-router-dom";
 import Root from "Root";
 
 import App from "./components/App";
 
+// (2) Now we need to include the main App as a router route
 ReactDOM.render(
   <Root>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </Root>,
   document.querySelector("#root")
 );
