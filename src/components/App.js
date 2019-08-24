3dbb16d1cdccca129d86dbe44de7d468dcc7ec0e
Andrew4d3 <andrew4d3@gmail.com>
2019-08-24 11:21:13 -0400

    Section 3 - Lecture 70

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from "react";
+// (1) We're going to add our App routes here, first we need to import the Route component from the react-router dependency
+import { Route } from "react-router-dom";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
-// For the moment, we are just rendering some text in the main App component
 export default () => {
+  // (2) And then we proceed to add the corresponding routes on this way. We also need to add the "exact" prop so that the route can match the root "/" path exactly!
   return (
     <div>
-      <CommentBox />
-      <CommentList />
+      <Route path="/post" component={CommentBox} />
+      <Route path="/" exact component={CommentList} />
     </div>
   );
 };
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,32 +1,25 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-
-//(1) Let's refactor this component to make it work as a class rather than as a function
+import { connect } from "react-redux";
 
 class CommentList extends Component {
-  // (3) This is the class method that will populate the list based on the props received
   renderComments() {
     return this.props.comments.map(comment => {
-      return <li key={comment}>{comment}</li>
-    })
+      return <li key={comment}>{comment}</li>;
+    });
   }
 
   render() {
-    // (2) Let's define the render method by displaying a HTML list, such list will be populated with another method
+    // (3) Let's add the following HTML title to the comment list for the sake of clarity
     return (
       <div>
-        <ul>
-          {this.renderComments()}
-        </ul>
+        <h4>Comment List</h4>
+        <ul>{this.renderComments()}</ul>
       </div>
-    )
+    );
   }
 }
-
-// (4) We need to map the comments from the global state to the props component
 function mapStateToProps(state) {
-  return { comments: state.comments }
+  return { comments: state.comments };
 }
 
-// (5) And connect everything
 export default connect(mapStateToProps)(CommentList);
