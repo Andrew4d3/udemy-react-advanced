b7fd53f2c0c974b16076ce100a2a425126a330a4
Andrew4d3 <andrew4d3@gmail.com>
2019-08-25 20:00:46 -0400

    Section 3 - Lecture 72 - Wiring up State

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
+// (1) We need to know whether the app is logged in or not. For that, we need to import the react-redux dependency
+import { connect } from "react-redux";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
-// (2) Let's refactor our Main App into a Class component
-export default class App extends Component {
-  renderButton() {}
-  // (3) Here we start building the header from our app which will contain the navigation links to each one of our routes
+// (2) We also need to refactor a little bit the App class component. So that we can export it at the button
+class App extends Component {
+  // (5) Now it's time to build our button based on the current auth state
+  renderButton() {
+    return this.props.auth ? (
+      <button>Sing Out</button>
+    ) : (
+      <button>Sing In</button>
+    );
+  }
+
   renderHeader() {
     return (
       <ul>
@@ -16,7 +25,6 @@ export default class App extends Component {
         <li>
           <Link to="/post">Post a comment</Link>
         </li>
-        {/* (4) Here we're going to call another render method that will display the SingIn button */}
         <li>{this.renderButton()}</li>
       </ul>
     );
@@ -24,9 +32,19 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        {/* (6) And don't forget to include the render method call inside the component's rendering */}
+        {this.renderHeader()}
         <Route path="/post" component={CommentBox} />
         <Route path="/" exact component={CommentList} />
       </div>
     );
   }
 }
+
+// (3) We need to map the auth state into the App's props
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+// (4) Here we connect the App with redux and export it
+export default connect(mapStateToProps)(App);
