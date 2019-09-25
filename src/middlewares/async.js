19789a9964eaae6eb62173d0256581db617649d7
Andrew Davis <reign-andrew@Andrews-MacBook-Pro.local>
2019-09-25 09:42:35 -0300

    Section 3 - Lecture 85 - Forwarding actions

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,4 +1,13 @@
-// (1) Our middleware function will be a 3-level High-order function, with the following arguments
 export default ({ dispatch }) => (next) => (action) => {
+    // (1) What we have to do...
+    // Check to see if the action
+    // has a promise on its 'payload' property
+    // If it does, then wait for it to resolve
+    // If it doesn't, then send the action to the
+    // next middleware
 
+    // (2) We neeed to check whether the action has a promise or not. If it doesn't, we forward it
+    if (!action.payload || !action.payload.then) {
+        return next(action);
+    }
 };
