72ad7b2ea85f201f691894cceb19aeccd0b802c2
Andrew4d3 <andrew4d3@gmail.com>
2019-09-25 22:02:13 -0300

    Section 3 - Lecture 86 - Waiting for Promise Resolution

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,13 +1,20 @@
 export default ({ dispatch }) => (next) => (action) => {
-    // (1) What we have to do...
     // Check to see if the action
     // has a promise on its 'payload' property
     // If it does, then wait for it to resolve
     // If it doesn't, then send the action to the
     // next middleware
 
-    // (2) We neeed to check whether the action has a promise or not. If it doesn't, we forward it
     if (!action.payload || !action.payload.then) {
         return next(action);
     }
+
+    // (1) This is what we want to do:
+    // We want to wait for the promise to resolve
+    // (get its data!!) and then create new action
+    // with that data and dispatch it
+    action.payload.then((response) => {
+        const newAction = { ...action, payload:response};
+        dispatch(newAction);
+    });
 };
