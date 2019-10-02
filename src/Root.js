8ea2b7a7f5338b26139df468a6619862dd44e17d
Andrew Davis <reign-andrew@Andrews-MacBook-Pro.local>
2019-10-02 10:11:18 -0300

    Section 4 - Lecture 92 - Emitting Warnings

--- src/Root.js
+++ src/Root.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux"; // (1) Let's import applyMiddleware so that we can integrate the redux-promise middleware
+import { createStore, applyMiddleware } from "redux";
 import async from 'middlewares/async';
+// (1) Now we need to wire up our recently created state validator. First, we import it
+import stateValidator from 'middlewares/stateValidator';
 import reducers from "reducers";
 
 export default ({ children, initialState = {} }) => {
-  // (2) Let's define the store as a constant
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(async) // (3) As third parameter, we pass the reduxPromise middleware, we apply it using the helper function we importerd before
+    // (2) Then we have to include it inside of the applyMiddleware function.
+    // We can either include it before or after the async middleware. It doesn't matter...
+    // The async middleware will execute not matther how you define the rest of the middlewares
+    applyMiddleware(async, stateValidator)
   );
 
   return <Provider store={store}>{children}</Provider>;
