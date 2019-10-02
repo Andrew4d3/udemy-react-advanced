6eec608243a99f5c8f57c83e54210a7b682e3a65
Andrew4d3 <andrew4d3@gmail.com>
2019-10-01 21:57:30 -0300

    Section 4 - Lecture 87 - Observing the middleware

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux"; // (1) Let's import applyMiddleware so that we can integrate the redux-promise middleware
-import reduxPromise from "redux-promise";
+import async from 'middlewares/async';
 import reducers from "reducers";
 
 export default ({ children, initialState = {} }) => {
@@ -9,7 +9,7 @@ export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(reduxPromise) // (3) As third parameter, we pass the reduxPromise middleware, we apply it using the helper function we importerd before
+    applyMiddleware(async) // (3) As third parameter, we pass the reduxPromise middleware, we apply it using the helper function we importerd before
   );
 
   return <Provider store={store}>{children}</Provider>;
