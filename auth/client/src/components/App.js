6fcbca84924f7384ea8f21ae9888a384a908fc15
Andrew4d3 <andrew4d3@gmail.com>
2019-10-06 22:03:45 -0300

    Section 6 - Lecture 124 - Wiring up React Router

--- auth/client/src/components/App.js
+++ auth/client/src/components/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-// (4) Now we need to import our recently created component
 import Header from "./Header";
 
-export default () => (
+// (6) Now let's destructure our App component props so we can pass out its children
+export default ({ children}) => (
   <div>
-    {/* (5) And include it here */}
     <Header />
-    Hi, I'm the app
+    {children}
   </div>
 );
-
-// (6) But we're getting an error: Invariant failed: You should not use <Link> outside a <Router>. Let's fix this next...
---
+++ auth/client/src/components/Welcome.js
@@ -0,0 +1,4 @@
+// (3) Let's now create a welcome component which only thing is going to do is to display a welcome message
+import React from "react";
+
+export default () => <h3>Welcome! Sing up or sign in!</h3>;
