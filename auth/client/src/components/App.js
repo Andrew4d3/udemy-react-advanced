8376ae944f7d067ff35cf60b6ac66c5a27411357
Andrew4d3 <andrew4d3@gmail.com>
2019-10-06 21:41:37 -0300

    Section 6 - Lecture 123 - Creating the Header

--- auth/client/src/components/App.js
+++ auth/client/src/components/App.js
@@ -1,4 +1,13 @@
 import React from "react";
+// (4) Now we need to import our recently created component
+import Header from "./Header";
 
-// (2) Now it's time to define the maim App component
-export default () => <div>Hi, I'm the app</div>;
+export default () => (
+  <div>
+    {/* (5) And include it here */}
+    <Header />
+    Hi, I'm the app
+  </div>
+);
+
+// (6) But we're getting an error: Invariant failed: You should not use <Link> outside a <Router>. Let's fix this next...
