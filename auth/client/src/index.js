1513ccf3c5f7837f73ce310f40e8e9f2899f6ca5
Andrew4d3 <andrew4d3@gmail.com>
2019-10-08 20:58:49 -0300

    Section 6 - Lecture 129 - Wiring up Middleware

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -1,17 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { Provider } from "react-redux";
+// (1) Now it's time to integrate redux-thunk which we're going to use to handle async calls. First lest import the applyMiddleware function from redux
+import { createStore, applyMiddleware } from "redux";
+// (2) And let's import redux-thunk
+import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import Welcome from "./components/Welcome";
-import Signup from './components/auth/Signup';
-import reducers from './reducers';
+import Signup from "./components/auth/Signup";
+import reducers from "./reducers";
 
-// (5) Finally, we wire up our reducers using the Provider tag and the createStore method
+// (3) Let's re-write our createStore implementation so that we have more space
+const store = createStore(
+  reducers,
+  {},
+  // (4) Here we include our redux-thunk middleware
+  applyMiddleware(reduxThunk)
+);
+
 ReactDOM.render(
-  <Provider store={createStore(reducers, {})}>
+  // (5) Now we just have to pass the store constant we defined above
+  <Provider store={store}>
     <BrowserRouter>
       <App>
         <Route path="/" exact component={Welcome} />
