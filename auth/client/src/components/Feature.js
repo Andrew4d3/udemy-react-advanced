815f4df74b08e7f7971bfd5ec9e20b0594d29388
Andrew4d3 <andrew4d3@gmail.com>
2019-10-13 12:46:31 -0300

    Section 6 - Lecture 138 - Feature Component

---
+++ auth/client/src/components/Feature.js
@@ -0,0 +1,10 @@
+// (1) So far, we are showing nothing in our feature path, so let's define a component for such route
+import React, { Component } from 'react';
+
+class Feature extends Component {
+	render() {
+		return <div>This is the feature!</div>;
+	}
+}
+
+export default Feature;
--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -1,34 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-// (1) Now it's time to integrate redux-thunk which we're going to use to handle async calls. First lest import the applyMiddleware function from redux
-import { createStore, applyMiddleware } from "redux";
-// (2) And let's import redux-thunk
-import reduxThunk from "redux-thunk";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
-import App from "./components/App";
-import Welcome from "./components/Welcome";
-import Signup from "./components/auth/Signup";
-import reducers from "./reducers";
+import App from './components/App';
+import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
+// (2) Don't forget to import the component inside the root
+import Feature from './components/Feature';
 
-// (3) Let's re-write our createStore implementation so that we have more space
+import reducers from './reducers';
+
 const store = createStore(
-  reducers,
-  {},
-  // (4) Here we include our redux-thunk middleware
-  applyMiddleware(reduxThunk)
+	reducers,
+	{},
+
+	applyMiddleware(reduxThunk)
 );
 
 ReactDOM.render(
-  // (5) Now we just have to pass the store constant we defined above
-  <Provider store={store}>
-    <BrowserRouter>
-      <App>
-        <Route path="/" exact component={Welcome} />
-        <Route path="/signup" component={Signup} />
-      </App>
-    </BrowserRouter>
-  </Provider>,
-  document.querySelector("#root")
+	<Provider store={store}>
+		<BrowserRouter>
+			<App>
+				<Route path="/" exact component={Welcome} />
+				<Route path="/signup" component={Signup} />
+				{/* (3) And include a new route for it */}
+				<Route path="/feature" component={Feature} />
+			</App>
+		</BrowserRouter>
+	</Provider>,
+	document.querySelector('#root')
 );
