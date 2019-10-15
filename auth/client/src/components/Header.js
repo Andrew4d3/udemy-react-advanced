72de1115ba7dee0876faca884e468cb45694b176
Andrew4d3 <andrew4d3@gmail.com>
2019-10-15 01:10:39 -0300

    Section 6 - Lecture 147 - Header Styling

--- auth/client/src/components/Header.js
+++ auth/client/src/components/Header.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// (1) The Header is showing all the nav options not matter wether the user is in session or not. It would be desireble to only show the appropriate options based on the auth state
-// For this, we need to connect the Header to redux
 import { connect } from 'react-redux';
 
+// (2) And import it on this way...
+import './HeaderStyle.css';
+
 class Header extends Component {
-	// (4) We need to create a Helper function which will only reder the correct links based on the authenticated state
 	renderLinks() {
 		return this.props.authenticated
 			? [
@@ -28,19 +28,17 @@ class Header extends Component {
 
 	render() {
 		return (
-			<div>
+			// (3) Now we can start marking our code with CSS classes
+			<div className="header">
 				<Link to="/">Redux Auth</Link>
-				{/* (5) Let's refactor this code so that we call the renderLinks methods we just implemented */}
 				{this.renderLinks()}
 			</div>
 		);
 	}
 }
 
-// (2) We need to get the auth state from redux
 function mapStateToProps(state) {
 	return { authenticated: state.auth.authenticated };
 }
 
-// (3) And connect the Header component like this
 export default connect(mapStateToProps)(Header);
