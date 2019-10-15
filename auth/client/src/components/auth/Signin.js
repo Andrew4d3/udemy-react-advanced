ee4ecc5a954b7b802e4daea9ab31609c656a3e08
Andrew4d3 <andrew4d3@gmail.com>
2019-10-15 00:26:43 -0300

    Section 6 - Lecture 144 - The Signin Action Component

---
+++ auth/client/src/components/auth/Signin.js
@@ -0,0 +1,55 @@
+// (1) Now it's to implement the Signin component. We're going to reuse most of the Signunp component code
+// Make sure to change all the signup references you find
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Signin extends Component {
+	onSubmit = formProps => {
+		this.props.signin(formProps, () => {
+			this.props.history.push('/feature');
+		});
+	};
+
+	render() {
+		const { handleSubmit } = this.props;
+		return (
+			<form onSubmit={handleSubmit(this.onSubmit)}>
+				<fieldset>
+					<label>Email</label>
+					<Field
+						name="email"
+						type="text"
+						component="input"
+						autoComplete="none"
+					/>
+				</fieldset>
+				<fieldset>
+					<label>Password</label>
+					<Field
+						name="password"
+						type="password"
+						component="input"
+						autoComplete="none"
+					/>
+				</fieldset>
+				<div>{this.props.errorMessage}</div>
+				<button>Sing In!</button>
+			</form>
+		);
+	}
+}
+
+function mapStateToProps(state) {
+	return { errorMessage: state.auth.errorMessage };
+}
+
+export default compose(
+	connect(
+		mapStateToProps,
+		actions
+	),
+	reduxForm({ form: 'signin' })
+)(Signin);
--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -9,8 +9,9 @@ import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
 import Feature from './components/Feature';
-// (2) Again, we import it
 import Signout from './components/auth/Signout';
+// (2) As usual, we import the new Signin component into the root react file
+import Signin from './components/auth/Signin';
 
 import reducers from './reducers';
 
@@ -29,8 +30,9 @@ ReactDOM.render(
 				<Route path="/" exact component={Welcome} />
 				<Route path="/signup" component={Signup} />
 				<Route path="/feature" component={Feature} />
-				{/* (3) And set a route for it */}
 				<Route path="/signout" component={Signout} />
+				{/* (3) Don't forget to define the Signin route */}
+				<Route path="/signin" component={Signin} />
 			</App>
 		</BrowserRouter>
 	</Provider>,
